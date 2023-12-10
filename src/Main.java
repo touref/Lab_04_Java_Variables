c87dbe9261510043bb821fb8359d2dfd81868f33
Fatou Toure <tourefo@mail.uc.edu>
2023-12-10 04:36:35 -0500

    I changed the file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+
+
+        int    numberkidsfamily = 6;
+        System.out.println("The number of kids in your family is: " + numberkidsfamily);
+        String isItraining = "Yes it's raining";
+        System.out.println("The answer to is it raining is: " + isItraining);
+        double priceGalgas = 3.95;
+        System.out.println("The price of a gallon of gas is: " + priceGalgas);
+        int favNum = 4;
+        System.out.println("Your favorite number is: " + favNum);
+        double shoesSize = 8.5;
+        System.out.println("Your shoes size is: " + shoesSize);
+        String birthMonth = "January";
+        System.out.println("Your birth month is: " + birthMonth);
+        String urName = "Fatou";
+        System.out.println("Your name is: " + urName);
+
+
+
+
+
+
+
+
+
 
     }
 }
